5fbf4b5b38ae83b4b5787854612029ec73edb2cc
NickLatkovich <nicklatkovich@gmail.com>
2021-07-06 21:53:07 +0300

    Attempt to fix disappearing highlights

--- Assets/Shashki/Scripts/CellComponent.cs
+++ Assets/Shashki/Scripts/CellComponent.cs
@@ -5,16 +5,24 @@ public class CellComponent : MonoBehaviour {
 	public KMSelectable Selectable;
 
 	private bool _highlighted = false;
-	public bool highlighted { get { return _highlighted; } private set { _highlighted = value; UpdateHighlight(); } }
+	public bool highlighted { get { return _highlighted; } private set { _highlighted = value; stepsToUpdateHighlight = 2; } }
 
 	private bool _selected = false;
-	public bool selected { get { return _selected; } set { _selected = value; UpdateHighlight(); } }
+	public bool selected { get { return _selected; } set { _selected = value; stepsToUpdateHighlight = 2; } }
 
+	private int stepsToUpdateHighlight = 1;
 	private GameObject highlight;
 
 	private void Start() {
 		Selectable.OnHighlight += () => { highlighted = true; };
 		Selectable.OnHighlightEnded += () => { highlighted = false; };
+	}
+
+	private void Update() {
+		if (stepsToUpdateHighlight > 0) {
+			stepsToUpdateHighlight -= 1;
+			UpdateHighlight();
+		}
 	}
 
 	private void UpdateHighlight() {
